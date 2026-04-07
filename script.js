1efa9221294f83678531a34058d690fbd51d86c9
Syed Tanvir Hossain <tanvirjoy04@gmail.com>
2026-04-08 01:17:32 +0600

    task-done

---
+++ script.js
@@ -0,0 +1,5 @@
+const crossEl = document.getElementById("cross");
+const bannerEl = document.getElementById("banner");
+crossEl.addEventListener('click', function(){
+  bannerEl.style.display = 'none'; 
+});
