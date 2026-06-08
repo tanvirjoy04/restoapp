c4e5d945a183890b0b222153ab9a46fbe498dfef
Syed Tanvir Hossain <tanvirjoy04@gmail.com>
2026-06-08 13:43:54 +0600

    fix minor bugs

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ const crossEl = document.getElementById("cross");
 const bannerEl = document.getElementById("banner");
 
 crossEl.addEventListener("click", function () {
-  // Start the fade out
   bannerEl.classList.add("hidden");
 
-  // Completely remove from layout after the 0.2s transition
   setTimeout(() => {
     bannerEl.style.display = "none";
   }, 200);
